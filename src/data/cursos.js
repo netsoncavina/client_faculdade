c2bde4545f509685a35c94362b5ffb0ddca2598c
Netson <netsoncavina@gmail.com>
2022-07-21 20:32:08 -0300

    adicionado arquivo com os nomes dos cursos e abreviações

---
+++ src/data/cursos.js
@@ -0,0 +1,10 @@
+export default cursos = {
+  0: {
+    nome: "Desenvolvimento de Software Multiplataforma",
+    abreviacao: "DSM",
+  },
+  1: {
+    nome: "Comércio Exterior",
+    abreviacao: "COMEX",
+  },
+};
