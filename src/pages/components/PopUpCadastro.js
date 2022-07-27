589aa5d698c91917ad41661c3842cc8c63b52275
Netson <netsoncavina@gmail.com>
2022-07-27 15:32:53 -0300

    mudança no botão para fechar o popup

--- src/pages/components/PopUpCadastro.js
+++ src/pages/components/PopUpCadastro.js
@@ -1,10 +1,12 @@
 import "./popup.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
 export default function PopUpCadastro(props) {
   return props.trigger ? (
     <div className="popup">
       <div className="popup-inner">
         <button className="close-btn" onClick={() => props.setTrigger(false)}>
-          Close
+          <FontAwesomeIcon icon={faXmark} size="2x" />
         </button>
         {props.children}
       </div>
